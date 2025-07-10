f28db981db732bc108237fae23df5fd13ce702bf
jimwell <jimuelmarcgensaya@gmail.com>
2025-07-10 14:23:35 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -27,6 +28,10 @@ const nextConfig: NextConfig = {
         hostname: '**.cloudworkstations.dev',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**',
       },
     ],
   },
